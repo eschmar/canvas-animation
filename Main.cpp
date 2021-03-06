f61fda68f827da13dcba973d41d7585ffd7ab697
Marcel Eschmann <marcel.eschmann@gmail.com>
2021-03-06 21:19:35 +0100

    Move to juce header

--- Main.cpp
+++ Main.cpp
@@ -1,80 +1,76 @@
 #include "MainComponent.h"
+#include <JuceHeader.h>
 
-//==============================================================================
-class GuiAppApplication  : public juce::JUCEApplication
-{
+class GuiAppApplication  : public juce::JUCEApplication {
 public:
-    //==============================================================================
     GuiAppApplication() {}
 
     // We inject these as compile definitions from the CMakeLists.txt
     // If you've enabled the juce header with `juce_generate_juce_header(<thisTarget>)`
     // you could `#include <JuceHeader.h>` and use `ProjectInfo::projectName` etc. instead.
-    const juce::String getApplicationName() override       { return JUCE_APPLICATION_NAME_STRING; }
-    const juce::String getApplicationVersion() override    { return JUCE_APPLICATION_VERSION_STRING; }
-    bool moreThanOneInstanceAllowed() override             { return true; }
-
-    //==============================================================================
-    void initialise (const juce::String& commandLine) override
-    {
-        // This method is where you should put your application's initialisation code..
-        juce::ignoreUnused (commandLine);
-
-        mainWindow.reset (new MainWindow (getApplicationName()));
+    const juce::String getApplicationName() override {
+        return ProjectInfo::projectName;
     }
 
-    void shutdown() override
-    {
-        // Add your application's shutdown code here..
+    const juce::String getApplicationVersion() override {
+        return ProjectInfo::versionString;
+    }
 
+    bool moreThanOneInstanceAllowed() override {
+        return true;
+    }
+
+    void initialise(const juce::String& commandLine) override {
+        // This method is where you should put your application's initialisation code..
+        juce::ignoreUnused(commandLine);
+
+        mainWindow.reset(new MainWindow (getApplicationName()));
+    }
+
+    void shutdown() override {
+        // Add your application's shutdown code here..
         mainWindow = nullptr; // (deletes our window)
     }
 
-    //==============================================================================
-    void systemRequestedQuit() override
-    {
+    void systemRequestedQuit() override {
         // This is called when the app is being asked to quit: you can ignore this
         // request and let the app carry on running, or call quit() to allow the app to close.
         quit();
     }
 
-    void anotherInstanceStarted (const juce::String& commandLine) override
-    {
+    void anotherInstanceStarted(const juce::String& commandLine) override {
         // When another instance of the app is launched while this one is running,
         // this method is invoked, and the commandLine parameter tells you what
         // the other instance's command-line arguments were.
         juce::ignoreUnused (commandLine);
     }
 
-    //==============================================================================
     /*
         This class implements the desktop window that contains an instance of
         our MainComponent class.
     */
-    class MainWindow    : public juce::DocumentWindow
-    {
+    class MainWindow : public juce::DocumentWindow {
     public:
-        explicit MainWindow (juce::String name)
-            : DocumentWindow (name,
-                              juce::Desktop::getInstance().getDefaultLookAndFeel()
-                                                          .findColour (ResizableWindow::backgroundColourId),
-                              DocumentWindow::allButtons)
-        {
+        explicit MainWindow(juce::String name) : DocumentWindow (
+            name,
+            juce::Desktop::getInstance().getDefaultLookAndFeel()
+                .findColour (ResizableWindow::backgroundColourId),
+                DocumentWindow::allButtons
+        ) {
             setUsingNativeTitleBar (true);
             setContentOwned (new MainComponent(), true);
 
-           #if JUCE_IOS || JUCE_ANDROID
+        #if JUCE_IOS || JUCE_ANDROID
             setFullScreen (true);
-           #else
+        #else
             setResizable (true, true);
             centreWithSize (getWidth(), getHeight());
-           #endif
+        #endif
 
             setVisible (true);
         }
 
-        void closeButtonPressed() override
-        {
+        void closeButtonPressed() override {
             // This is called when the user tries to close this window. Here, we'll just
             // ask the app to quit when this happens, but you can change this to do
             // whatever you need.
@@ -96,6 +92,5 @@ private:
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-//==============================================================================
 // This macro generates the main() routine that launches the app.
 START_JUCE_APPLICATION (GuiAppApplication)
